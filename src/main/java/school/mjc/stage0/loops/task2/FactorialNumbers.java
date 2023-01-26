7a8f373d1fa6015dc5f1122eec7642517e35d30d
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-01-26 21:22:34 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -3,6 +3,20 @@ package school.mjc.stage0.loops.task2;
 public class FactorialNumbers {
 
     public void printFactorialRow(int printToInclusive) {
+        int i = 0;
+        int a = 2;
+        while (i < printToInclusive){
+            if (i == 0){
+                System.out.println(0);
+            }
+            else if (i == 1){
+                System.out.println(1);
+            }
+            else {
+                a *= (i+1);
+                System.out.println(a);}
+            i++;
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -4,13 +4,7 @@ import java.sql.SQLOutput;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        int i =0;
-        String s;
-        while (i < chars.length ){
-            s = String.valueOf(new StringBuilder().append(chars[i]));
-            i++;
-        }
-        System.out.println(s);
+
 
     }
 }
